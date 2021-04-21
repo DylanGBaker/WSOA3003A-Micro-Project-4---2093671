2375b7bbd1f2ca94a5f7df083014d6613e82071c
Skaduvee <2093671@students.wits.ac.za>
2021-04-21 17:48:34 +0200

    Unity Project Folder

--- WSOA3003A Micro-Project 4/Assets/Scripts/Platform Levels Scripts/EnemyController.cs
+++ WSOA3003A Micro-Project 4/Assets/Scripts/Platform Levels Scripts/EnemyController.cs
@@ -54,7 +54,7 @@ public class EnemyController : MonoBehaviour
 
     public IEnumerator Die()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.2f);
         Destroy(gameObject);
     }
 }
